96b6144bf0eba3977f0cbc9c124cd1e76b51a375
outsider <515885633@qq.com>
2023-07-26 18:06:10 +0800

    完善描述；

--- src/packages/components/Informations/Mores/Descriptions/index.vue
+++ src/packages/components/Informations/Mores/Descriptions/index.vue
@@ -8,61 +8,82 @@
 -->
 <template>
     <n-descriptions label-placement="top" bordered :column="6">
-      <n-descriptions-item>
+      <n-descriptions-item v-for="(value, key) in data">
         <template #label>
-          早餐
+          {{ value.label }}
         </template>
-        苹果
-      </n-descriptions-item>
-      <n-descriptions-item label="早午餐">
-        苹果
-      </n-descriptions-item>
-      <n-descriptions-item label="午餐">
-        苹果
-      </n-descriptions-item>
-      <n-descriptions-item label="晚餐">
-        苹果
-      </n-descriptions-item>
-      <n-descriptions-item label="正餐">
-        苹果
-      </n-descriptions-item>
-      <n-descriptions-item label="夜宵">
-        苹果
-      </n-descriptions-item>
-      <n-descriptions-item label="苹果">
-        苹果
-      </n-descriptions-item>
-      <n-descriptions-item label="苹果" :span="2">
-        苹果
-      </n-descriptions-item>
-      <n-descriptions-item label="苹果" :span="3">
-        苹果
+        {{ value.value }}
       </n-descriptions-item>
     </n-descriptions>
   </template>
   
   <script setup lang="ts">
   import { CreateComponentType } from '@/packages/index.d'
-  import { PropType, reactive, toRefs } from 'vue';
+  import { onMounted, PropType, reactive, ref, toRefs, watch } from 'vue';
 
-    const props = defineProps({
-    chartConfig: {
-        type: Object as PropType<CreateComponentType>,
-        required: true
+  const props = defineProps({
+  chartConfig: {
+      type: Object as PropType<CreateComponentType>,
+      required: true
+  }
+  })
+
+  const { w, h } = toRefs(props.chartConfig.attr)
+  
+  const { rowNum, unit, color, textColor, borderColor, indexFontSize, leftFontSize, rightFontSize } = toRefs(
+    props.chartConfig.option
+  )
+
+  const status = reactive({
+    mergedConfig: props.chartConfig.option,
+    rowsData: [],
+    animationIndex: 0,
+    animationHandler: 0,
+    updater: 0
+  })
+
+  const data = ref({})
+
+  const calcData = () => {
+    let { dataset, rowNum, sort } = status.mergedConfig
+    data.value = dataset
+  }
+
+  const onRestart = async () => {
+  try {
+    if (!status.mergedConfig) return
+    //stopAnimation()
+    calcData()
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+  watch(
+    () => w.value,
+    () => {
+      onRestart()
     }
-    })
+  )
 
-    const { w, h } = toRefs(props.chartConfig.attr)
-    
-    const { rowNum, unit, color, textColor, borderColor, indexFontSize, leftFontSize, rightFontSize } = toRefs(
-      props.chartConfig.option
-    )
+  watch(
+    () => h.value,
+    () => {
+      onRestart()
+    }
+  )
 
-    const status = reactive({
-      mergedConfig: props.chartConfig.option,
-      rowsData: [],
-      animationIndex: 0,
-      animationHandler: 0,
-      updater: 0
-    })
+  // 数据更新
+  watch(
+    () => props.chartConfig.option,
+    () => {
+      onRestart()
+    },
+    { deep: true }
+  )
+
+  onMounted(() => {
+    onRestart()
+  })
   </script>
